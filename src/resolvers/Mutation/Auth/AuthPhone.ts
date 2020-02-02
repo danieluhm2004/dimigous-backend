4c0763cea882d5228c4cd4788ab6654cebd7e5d9
다니엘 <36157019+danieluhm2004@users.noreply.github.com>
2020-02-03 02:32:38 +0900

    Update AuthPhone.ts

--- src/resolvers/Mutation/Auth/AuthPhone.ts
+++ src/resolvers/Mutation/Auth/AuthPhone.ts
@@ -3,19 +3,13 @@ import MessageUtil from '../../../utils/MessageUtil';
 import Phone from '../../../models/Phone';
 
 export default async function(_: any, { phone }: { phone: string }) {
-  console.log('asdasdas');
   if (!phone.match(RegexUtil.PHONE))
     throw new Error('올바른 전화번호를 입력해야 해요');
-  console.log('asdasdas');
-
   const randomCode = Math.floor(100000 + Math.random() * 900000);
-  console.log('asdasdas');
-
   const { messageId, requestId } = await MessageUtil(
     `요청하신 인증번호는 [${randomCode}] 입니다.`,
     phone,
   );
-  console.log('asdasdas');
 
   await Phone.create({
     phone,
